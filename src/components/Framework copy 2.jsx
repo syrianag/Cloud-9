ee9002ea4f005cc5e051836efc49175f500d3d39
Sean <state0221@launchpadphilly.org>
2025-11-06 14:04:05 -0500

    Git commit -m "Deleted unnecessary duplicate file 'Framework copy 2.jsx'"

--- src/components/Framework copy 2.jsx
+++
@@ -1,8 +0,0 @@
-export default function Framework() {
-  return (
-    <div className="framework">
-      <h2>Framework Component</h2>
-      <p>This is the Framework component content.</p>
-    </div>
-  );
-}
